0625a8e0b137e59b738b71fc28ccc8e246cae0be
tomkovich <tomkovich.ya@yandex.ru>
2019-09-30 22:37:20 +0300

    files

---
+++ app/js/tom.slider.js
@@ -0,0 +1,167 @@
+let tomSlider = function(settings) {
+	
+	let defaultSettings = {
+		selector: '.tom-slider',
+		margin: '0',
+		arrowNext: '.arrowNext', 
+		arrowPrev: '.arrowPrev',
+		innerArrowNext: '>',
+		innerArrowPrev: '<',
+		slidesShow: 1,
+		autoplay: false
+	}
+
+	if(settings === undefined) {
+		settings = defaultSettings;
+	}
+
+	settings = {
+		selector: settings.selector || '.tom-slider',
+		margin: settings.margin || '0',
+		arrowNext: document.querySelector(settings.arrowNext) || document.querySelector('.arrowNext'), 
+		arrowPrev: document.querySelector(settings.arrowPrev) || document.querySelector('.arrowPrev'),
+		innerArrowNext: settings.innerArrowNext || '>',
+		innerArrowPrev: settings.innerArrowPrev || '<',
+		slidesShow: settings.slidesShow || 1,
+		autoplay: settings.autoplay
+	}
+
+	let nextSlideButton = settings.arrowNext;
+	let prevSlideButton = settings.arrowPrev;
+
+	let re = /[0-9]/g;
+	let marginNumber = settings.margin.match(re).join('');
+	marginNumber = marginNumber * 2 / 10;
+
+	let _carousel = () => {
+
+		nextSlideButton.innerHTML = settings.innerArrowNext;
+		prevSlideButton.innerHTML = settings.innerArrowPrev;
+
+		let selector = document.querySelector(settings.selector);
+		
+
+		let slideWidth = (100 / settings.slidesShow) - marginNumber + '%';
+
+		let carouselWrapper = selector.firstElementChild;
+		carouselWrapper.classList.add('tom-slides');
+
+		let carouselSlides = carouselWrapper.children;
+		let slides = [];
+
+		for( let i = 0; i < carouselSlides.length; i++) {
+			slides.push(carouselSlides[i]);
+		}
+
+		let activeSlides = slides.slice(0, settings.slidesShow);
+
+		activeSlides.forEach( slide => {
+			slide.classList.add('active');
+			slide.style.margin = settings.margin;
+		});
+
+		slides.forEach( slide => {
+			slide.style.width = slideWidth;
+			slide.classList.add('tom-slide');
+		});
+		
+		let count = settings.slidesShow - 1;
+
+		let autoHeight = () => {
+			let slides = document.querySelectorAll('.tom-slide.active');
+
+			let arr = [];
+
+			slides.forEach((slide) => {
+				arr.push(slide.offsetHeight);
+			});
+
+			let whoisbigger = (a, b) => {
+				if( a < b) return 1;
+				if( a > b ) return -1;
+			}
+
+			arr.sort(whoisbigger)
+			console.log(settings.selector)
+			selector.style.height = `${arr[0] * 1.3}px`
+		}
+
+		autoHeight();
+
+		let slideNext = () => {
+
+			let currentSlide = carouselSlides[count];
+			let nextSlide = currentSlide.nextElementSibling;
+			let actives = document.querySelectorAll('.tom-slide.active');
+
+			actives[0].classList.remove('active');
+			actives[0].style.margin  = '0';
+
+			if(nextSlide === null) {
+				count = settings.slidesShow - 1;
+
+				for(let i = 0; i < carouselSlides.length; i++) {
+					carouselSlides[i].classList.remove('active');
+					carouselSlides[i].style.margin  = '0';
+				}
+				activeSlides.forEach( slide => {
+					slide.classList.add('active')
+					slide.style.margin  = settings.margin;
+				});
+				
+			} else {
+				nextSlide.style.margin  = settings.margin;
+				nextSlide.classList.add('active');
+				count++;
+			}
+
+		}
+
+		let slidePrev = () => {
+
+			let currentSlide = carouselSlides[count];
+			let actives = document.querySelectorAll('.tom-slide.active');
+			let prevSlide = actives[0].previousElementSibling;
+
+			actives[actives.length - 1].classList.remove('active');
+			actives[actives.length - 1].style.margin  = '0';
+
+			if(prevSlide === null) {
+
+				count = slides.length - 1;
+
+				for(let i = count; i >= 0; i--) {
+					slides[i].classList.remove('active');
+					slides[i].style.margin = '0';
+				}
+
+				slides.forEach( (slide, index) => {
+					if(index >= slides.length - settings.slidesShow) {
+						slide.classList.add('active');
+						slide.style.margin  = settings.margin;
+					} 
+				});
+				
+			} else {
+				prevSlide.style.margin  = settings.margin;
+				prevSlide.classList.add('active');
+				count--;
+			}
+
+		}
+
+		settings.arrowNext.addEventListener('click', slideNext);
+		settings.arrowPrev.addEventListener('click', slidePrev);
+
+		if(settings.autoplay) {
+
+			setInterval( function() {
+				slideNext();
+			}, 2500);
+		}
+
+	} 
+
+	return _carousel();
+}
+
